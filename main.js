3e04814c82b2e1ae5ff053193ff90feab0afac71
Your Name <you@example.com>
2026-09-18 12:45:21 +0500

    feat: add main page

--- main.js
+++ main.js
@@ -6,4 +6,54 @@ $(document).ready(function () {
             $('header').removeClass('scrolled');
         }
     });
+    $('.slick-slider').each(function () {
+        let slider = $(this);
+
+        let options = {
+            prevArrow: slider.parent().find('.slider-prev'),
+            nextArrow: slider.parent().find('.slider-next'),
+            infinite: true,
+            autoplay: true,
+            dots: false
+        };
+
+        let extraOptions = {};
+
+        if (slider.hasClass('directions')) {
+            extraOptions = {
+                slidesToShow: 3.3,
+                slidesToScroll: 1,
+                centerMode: false,
+                arrows: true,
+                dots: false,
+                autoplay: false,
+                responsive: [
+                    {
+                        breakpoint: 1200,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 1,
+                        }
+                    },
+                    {
+                        breakpoint: 992,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 1,
+                        }
+                    },
+                    {
+                        breakpoint: 576,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                        }
+                    },
+                ]
+            };
+        }
+
+
+        slider.slick($.extend({}, options, extraOptions));
+    });
 });
